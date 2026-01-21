6bfb0833890ea88bcc93d72578b4f1fd2abb53ed
VMaheshwari16 <131899919+VMaheshwari16@users.noreply.github.com>
2026-01-21 15:53:21 +0530

    Create user.php

---
+++ data/user.php
@@ -0,0 +1,12 @@
+<?php
+return [
+[ "username" => "Mahesh",
+ "email" => "mahesh@gmail.com",
+ "password" => "16072003"
+],
+[ "username" => "Raja",
+ "email" => "Raja@gmail.com",
+ "password" => "0908"
+]
+];
+?>
